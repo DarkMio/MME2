9b8388551328e183df8f86f9b2d3bcb001745e74
Mio Bambino <s59330@beuth-hochschule.de>
2016-10-16 12:32:51 +0200

    material design update

--- js/app.js
+++ js/app.js
@@ -6,13 +6,47 @@ window.onload = function() {
 };
 
 
+function initVideo2() {
+    var frames = document.getElementsByClassName('videoFrame');
+    /*
+     Basic structure of n-Frames
+        + div .videoFrame
+        |
+        +- video
+        |
+        +-+ ul .videoControls
+          |
+          + li (play)
+          + li (pause)
+          * li (+ / -)
+     */
+
+
+}
+
+
+function iterator(iterable) {
+    // @TODO: check for iteration first
+
+    var iterable = iterable;
+    var count = 0;
+    var max = iterable.length;
+    return {
+        next: function() {
+            if(count >= max) {
+                return null;
+            }
+            return iterable[count++];
+        }
+    }
+}
+
 function initVideo() {
     var videos = document.getElementsByTagName('video');
     console.log(videos);
     for(var i = 0; i < videos.length; i++) {
         var video = videos.item(i);
         video.onclick = function(){
-            console.log(this);
             if(this.paused) {
                 this.play();
                 // this.parentNode.className = "videoItem play";
