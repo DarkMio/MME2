4ab21e71118ac76b186caabf31af228bdff9c54b
DarkMio <s59330@beuth-hochschule.de>
2016-10-25 15:17:50 +0200

    Assignment 2 - Done.

--- A2/app.js
+++ A2/app.js
@@ -1,14 +1,45 @@
 "use strict";
 var express = require('express');
 var path = require('path');
-var app = express();
+var fs = require('fs');
+var fileMemo = function() { // obviously no callback hell.
+    var memo = null;
+    return function (callback) {
+        if(memo === null) {
+            fs.readFile("./public/data/file.txt", 'utf8', function(err, contents) {
+                if(err) {
+                    throw err;
+                }
+                memo = contents;
+                callback(memo);
+            });
+        } else {
+            callback(memo);
+        }
+    }
+}();
 
-app.get('/', function(req, res) {
-    console.log("> Serving new request.");
+var app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/test', function(req, res) {
+    console.log("> Serving new request from " + req.connection.remoteAddress);
     res.send('Hello world.');
 });
+app.get('/time', function(req, res) {
+    res.setHeader('content-type', 'text/plain');
+    res.send(new Date().getTime() + "");
+});
+app.get('/file.txt', function(req, res) {
+    var begin = process.hrtime()[1];
+    fileMemo(function(result) {
+        var end = process.hrtime()[1] - begin;
+        res.setHeader('content-type', 'text/plain');
+        res.send("Done: " + end + "ns\n" + result);
+    });
 
-var server = app.listen(3000, function(){
+});
+
+app.listen(3000, function(){
     console.log("Listening on port 3000.");
 });
 
